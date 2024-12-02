b9366114f4cda70a93e87eaf78987c6f6a559670
DanG456 <daniel.gil1922@gmail.com>
2024-12-02 17:12:22 -0600

    Creacion de paginas bases para comenzar el proyecto

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <NavBarMenu />
+        <h1>Home page</h1>
+    </div>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Pagina para login</h1>
+    </div>
+</template>
